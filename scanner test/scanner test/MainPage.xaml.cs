9c13075946cb24bba4a5692ef6f40cd40efc0e97
Casper Koksby Møller Nielsen <casp8660@zbc.dk>
2021-06-10 08:47:07 +0200

    Add project files.

---
+++ scanner test/scanner test/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace scanner_test
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Scanner_OnScanResult(ZXing.Result result)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                CodeLabel.Text = result.Text + " " + result.BarcodeFormat.ToString();
+            });
+        }
+    }
+}
